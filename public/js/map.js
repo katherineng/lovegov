4b091e9f3d49c41311caa0fd271aec36bd06ae68
Katherine Ng <kwng@cs.brown.edu>
2013-03-21 17:05:16 -0400

    skeleton files

---
+++ public/js/map.js
@@ -0,0 +1,11 @@
+function initialize() {
+    var mapOptions = {
+        center: new google.maps.LatLng(-38.6, -98),
+        zoom: 8,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+    var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);
+
