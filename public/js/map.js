5f9ce7ceafaa87fae5f68a18c5d7a03cb3e19ed7
Katherine Ng <kwng@cs.brown.edu>
2013-04-08 19:37:33 -0400

    creating basic index page, loading maps API

--- public/js/map.js
+++ public/js/map.js
@@ -1,7 +1,7 @@
 function initialize() {
     var mapOptions = {
-        center: new google.maps.LatLng(-38.6, -98),
-        zoom: 8,
+        center: new google.maps.LatLng(38.6, -98),
+        zoom: 4,
         mapTypeId: google.maps.MapTypeId.ROADMAP
     };
     var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
@@ -9,3 +9,7 @@ function initialize() {
 
 google.maps.event.addDomListener(window, 'load', initialize);
 
+
+$(document).ready(function() {
+
+});
---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var engines = require('consolidate');
+var app = express();
+
+var anyDB = require('any-db');
+var conn = anyDB.createConnection();
+
+app.use(express.bodyParser());
+app.use('/public', express.static(__dirname + '/public'));
+app.use(express.logger('dev'));
+
+app.listen(8080, function() {
+    console.log('- Server listening on port 8080');
+});
